00016ee990762542f96cfc9fd61a72f7de2bb918
Darrylwin <logossoudarryl20@gmail.com>
2024-05-18 01:25:28 +0000

    ajout d'autres users

--- Foodly.sql
+++ Foodly.sql
@@ -53,3 +53,10 @@ VALUES(
     "Ike",
     "Ike_amb@gmail.com"
 );
+
+INSERT INTO `utilisateur` (`nom`, `prenom`, `email`)
+VALUES
+('Doe', 'John', 'john@yahoo.fr'),
+('Smith', 'Jane', 'jane@hotmail.com'),
+('Dupont', 'Sebastien', 'sebastien@orange.fr'),
+('Martin', 'Emilie', 'emilie@gmail.com');
